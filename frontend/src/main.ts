99a8cbf0c35bca5b10b44146b6d9a0de8d12a28b
Julia Teixeira <juuhc.teixeira@gmail.com>
2023-11-21 12:20:19 +0000

    feature: inicia componente de login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,9 @@
 * Material Dashboard Angular - v2.8.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
+* Product Page: https://www.creative-tim.com/product/sf2
 * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular2/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/sf2/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
 
